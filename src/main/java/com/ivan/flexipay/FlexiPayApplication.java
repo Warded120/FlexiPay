021a9510e203387c245a026e77e781ac3b51dd8a
Warded120 <greenvania11@gmail.com>
2025-02-26 15:32:49 +0200

    initial commit

---
+++ src/main/java/com/ivan/flexipay/FlexiPayApplication.java
@@ -0,0 +1,13 @@
+package com.ivan.flexipay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlexiPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlexiPayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ivan/flexipay/FlexiPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivan.flexipay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlexiPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
